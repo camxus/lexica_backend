6e40bfea0982d804bc560c579a81e6ff9440a316
Camillus Konkwo <camillus.konkwo@gmail.com>
2026-01-05 17:50:43 +0100

    Updated usersRoutes.ts

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -11,6 +11,5 @@ const router: Router = express.Router();
 
 router.get("/profile", authenticate, getProfile);
 router.put("/profile", authenticate, uploadAvatar, updateProfile);
-router.get("/progress", authenticate, getUserProgress);
 
 export default router;
